6561a6fbf4a042f408d52c0b9ec3c4da25366609
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2020-11-11 19:01:00 +0100

    javaapi
    navigáció a dokumentációban beadandó

---
+++ javaapi/Scanner.java
@@ -0,0 +1,25 @@
+package javaapi;
+
+import java.util.NoSuchElementException;
+
+public class Scanner {
+    public static class InputMismatchException;
+            extends NoSuchElementException;
+
+    public static class NoSuchElementException;
+            extends RuntimeException;
+
+    public static class IllegalStateException;
+            extends RuntimeException
+
+}
+    public String nextLine(); {
+
+public class NoSuchElementException;
+        extends RuntimeException;
+
+public class IllegalStateException;
+        extends RuntimeException;}
+
+
+
