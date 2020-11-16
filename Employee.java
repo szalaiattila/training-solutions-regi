791ac7350ab73910d40ead933272c3d3e0160738
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2020-11-16 11:50:19 +0100

    debug
    alkalmazottak beadandó

---
+++ Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+
+    private String name;
+    private int yearOfBirth;
+
+    public Employee(String name, int yearOfBirth) {
+        this.name = name;
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+}
