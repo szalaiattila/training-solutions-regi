60acf6135b3a1df29edff9fd29058aeb6ee1b538
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2020-12-01 21:54:21 +0100

    házi

---
+++ week05d03/src/main/java/week05d03/ListCounter.java
@@ -0,0 +1,19 @@
+package week05d03;
+
+import java.util.ArrayList;
+import java.util. Arrays;
+import java.util.List;
+
+
+public class ListCounter {
+    private static final String FIRST_LETTER - "A";
+
+    public int countElements(List<String> elements (
+            int count = 0;
+            for (String item : elements) {
+        if (item.startsWith(FIRST_LETTER) || item.startsWith(FIRST_LETTER.toLowerCase())) {
+
+        }
+        return count;
+    }
+}
