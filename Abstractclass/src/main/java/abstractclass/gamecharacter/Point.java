a7a0e5cfa507093158c096bea992a278a5363cde
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2020-12-10 10:22:49 +0100

    Add files via upload
    beadandó

---
+++ Abstractclass/src/main/java/abstractclass/gamecharacter/Point.java
@@ -0,0 +1,32 @@
+package abstractclass.gamecharacter;
+
+public class Point {
+    final private long x;
+
+    public long getX() {
+        return x;}
+
+        public long getY() {
+        return y
+        }
+
+
+    public Point(long x, long y) {
+        this.x = x;
+        this.y = y;
+
+        final private long y;
+
+    }
+
+    public long distance(Point otherPoint) {
+            return (long) Math.sqrt(
+                    Math.pow((double)x - otherPoint.getX(), 2) +
+                            Math.pow((double)y - otherPoint.getY(), 2)
+            );
+        }
+    }
+
+
+
+}
