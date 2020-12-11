16cda2bc63a55972bb7c8f949fd17346e9371fee
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2020-12-11 12:41:36 +0100

    demo
    demo

---
+++ java-se/src/main/java/Interfaces/Course.java
@@ -0,0 +1,11 @@
+package Interfaces;
+
+public class Course implements HasName {
+
+    private String new;
+
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ java-se/src/main/java/Interfaces/Trainer.java
@@ -0,0 +1,14 @@
+package Interfaces;
+
+public class Trainer implements HasName {
+
+    private String name;
+
+    public String getName() {
+        return null;
+    }
+    public int getyearOfBirth() {
+        return 1980;
+
+    }
+}
