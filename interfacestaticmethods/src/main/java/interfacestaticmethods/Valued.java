97b3459098176ab44269a5817747663abe93af1f
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2020-12-16 21:07:09 +0100

    Add files via upload
    beadandó

---
+++ interfacestaticmethods/src/main/java/interfacestaticmethods/Valued.java
@@ -0,0 +1,18 @@
+package interfacestaticmethods;
+
+public interface Valued {
+    import java.util.List;
+
+    public interface Valued {
+
+        static double totalValue(List<Valued> items) {
+            double result = 0;
+            for (Valued valued : items) {
+                result += valued.getValue();
+            }
+            return result;
+        }
+
+        double getValue();
+    }
+}
