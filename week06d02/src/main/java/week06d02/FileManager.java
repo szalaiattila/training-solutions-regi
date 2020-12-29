e7a1c939cc304730ab400a8bc7aa43cb430a6760
szalaiattila <73574745+szalaiattila@users.noreply.github.com>
2020-12-29 15:33:33 +0100

    Add files via upload

---
+++ week06d02/src/main/java/week06d02/FileManager.java
@@ -0,0 +1,14 @@
+package week06d02;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class FileManager {
+
+    public void readFromFile(String fileName) throws IOException{
+
+        Scanner scanner = new Scanner(Path.of(fileName));
+
+    }
+}
